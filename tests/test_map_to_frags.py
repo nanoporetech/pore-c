b9518446d6c91d5500e850b5392e7bac29a65667
Matthew Pendleton <matthew.pendleton@nanoporetech.com>
2019-01-04 15:39:57 -0500

    First commit of code. No testing has been done but all the parts should be largely there.

---
+++ tests/test_map_to_frags.py
@@ -0,0 +1,28 @@
+import pytest
+from pore_c.tools.map_to_frags import load_reference, assign_to_fragments, porec_iterator, map_to_fragments
+
+def test_load_reference():
+    pass
+
+def test_assign_to_fragments():
+    pass
+
+def test_porec_iterator():
+    pass
+
+def test_map_to_fragments():
+    pass
+
+
+#def test_read_mappings_iter(namesorted_align_file):
+#    res = [len(aligns) for aligns in read_mappings_iter(namesorted_align_file)]
+#    assert(res == [2, 5, 6, 5, 2, 1])
+
+
+#def test_cluster_aligned_segments(namesorted_align_file):
+
+#    aligns = list(namesorted_align_file)[2:7]
+#    keep = cluster_aligned_segments(aligns, 20)
+#    assert(keep == [0, 1, 3])
+
+
