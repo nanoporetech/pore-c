f9cf47b3666a8b44c6357cca218703d34978327f
Matthew Pendleton <matthew.pendleton@nanoporetech.com>
2019-01-07 16:47:31 -0500

    pytesting complete for map_to_frags.py.

--- tests/test_generate_fragments.py
+++ tests/test_generate_fragments.py
@@ -1,7 +1,5 @@
 import pytest
-import re
 from pore_c.tools.generate_fragments import create_regex, revcomp, find_site_positions
-
 
 @pytest.mark.parametrize("pattern,rev_pattern",[
     ('ACGT', 'ACGT'),
@@ -15,9 +13,9 @@ def test_revcomp(pattern, rev_pattern):
 
 
 @pytest.mark.parametrize("pattern,regex",[
-    ('AAGCTT', '(AAGCTT|AAGCTT)'),
-    ('AAGNCTT', '(AAG[ACGT]CTT|AAG[ACGT]CTT)'),
-    ('(GAATTC|GCGGCCGC)', '((GAATTC|GCGGCCGC)|(GCGGCCGC|GAATTC))')
+    ('AAGCTT', '(AAGCTT)'),
+    ('AAGNCTT', '(AAG[ACGT]CTT)'),
+    ('(GAATTC|GCGGCCGC)', '(GAATTC|GCGGCCGC)')
 ])
 def test_create_regex(pattern, regex):
     assert(regex == create_regex(pattern).pattern)
@@ -26,11 +24,14 @@ def test_create_regex(pattern, regex):
 @pytest.mark.parametrize("regex,seq,positions",[
     ('(ACAGTA|TACTGT)', "ACAGTATACTGT", [0, 6]),
     ('(ACAGTA|TACTGT)', "ACAGAATACAGT", []),
+    ('(ACAG[ACGT]A|TACTGT)', "ACAGAATACAGT", [0]),
+    ('(ACAGTA|TAC[ACGT]GT)', "ACAGAATACAGT", [6]),
     ('(AGCT|AAGCTT)', 'AAGCTT', [0]),
-    ('(AAGCTT|AAGCTT)', 'AAGCTT', [0]),
-    ('(AAGCTT|AAGCTT)', 'NAAGCTT', [1]),
-    ('(AAGCTT|AAGCTT)', 'AAGCTTAAGCTT', [0,6]),
+    ('(AAGCTT)', 'AAGCTT', [0]),
+    ('(AAGCTT)', 'NAAGCTT', [1]),
+    ('(AAGCTT)', 'AAGCTTAAGCTT', [0,6]),
 ])
 def test_find_positions(regex, seq, positions):
+    import re
     assert(positions == find_site_positions(re.compile(regex), seq))
 
--- tests/test_map_to_frags.py
+++ tests/test_map_to_frags.py
@@ -1,7 +1,7 @@
 import pytest
-from pore_c.tools.map_to_frags import load_reference, assign_to_fragments, porec_iterator, map_to_fragments
+import re
+from pore_c.tools.map_to_frags import load_reference, assign_to_fragment, porec_iterator, map_to_fragments
 from pore_c.tools.map_to_frags import Contact, Cwalk
-
 
 def test_load_reference(hicREF_file):
     ref_sizes, ref_IDs = load_reference(hicREF_file)
@@ -21,31 +21,29 @@ def test_load_reference(hicREF_file):
     for positions in ref_sizes.values():
         assert sorted(positions) == positions
 
-
-
-
 #signature: assign_to_fragment(ref_frags: tuple, ref_IDs: dict, loc: tuple, method: str) -> NamedTuple:
 #This is the function that actually does the assignment. map_to_fragments is a wrapper for it 
 #  that manages bam reading and fileIO, whose internal code is here for management purposes
-def test_assign_to_fragments(hicREF_file, namesorted_align_file):
+#TODO [ ]: future testing should include different methods
+def test_assign_to_fragment(hicREF_file, namesorted_align_filename):
     ref_sizes, ref_IDs = load_reference(hicREF_file)
 
     results = []
-    for readID, aligns in porec_iterator(namesorted_align_file):
-        walk = Cwalk(name)
+    for readID, aligns in porec_iterator(namesorted_align_filename):
+        walk = Cwalk(readID)
         for monomer in aligns:
             loc = (monomer.reference_name, monomer.reference_start,monomer.reference_end)
-            frag,pos = assign_to_fragment(ref_frags, ref_IDs, loc, method = method)
+            frag,pos = assign_to_fragment(ref_sizes, ref_IDs, loc, 'start') 
             new_contact = Contact(monomer.reference_name, frag, monomer.is_reverse, pos, monomer.mapping_quality)
             walk.add(new_contact)
         results.append(len(str(walk).split()))
 
     assert results == [11, 26, 21, 21, 11, 6]
 
-def test_porec_iterator(namesorted_align_file):
-    res = dict([aligns for aligns in porec_iterator(namesorted_align_file)])
-    assert res.keys() == ["0a4cf95d-1410-42b1-867c-68e987123675","0a6b6929-f064-42d4-afd4-24b38a966d03","0a0030d2-60a6-4498-8ea0-329d9dcac82b","0aa14eb3-bdd8-40eb-97c3-d37c6eb99ef6","0af7db4f-2804-4dc8-aae9-fc4611008d4a","0b72d767-a437-43e2-a9ea-717d99864688"]
-    assert([len(align) for align in res.values()] = [2, 5, 6, 5, 2, 1])
+def test_porec_iterator(namesorted_align_filename):
+    res = dict([aligns for aligns in porec_iterator(namesorted_align_filename)])
+    assert sorted(res.keys()) == ['0a0030d2-60a6-4498-8ea0-329d9dcac82b', '0a4cf95d-1410-42b1-867c-68e987123675', '0a6b6929-f064-42d4-afd4-24b38a966d03', '0aa14eb3-bdd8-40eb-97c3-d37c6eb99ef6', '0af7db4f-2804-4dc8-aae9-fc4611008d4a', '0b72d767-a437-43e2-a9ea-717d99864688']
+    assert([len(align) for align in res.values()] == [2, 5, 6, 5, 2, 1])
 
     #check that all the alignments in each read bundle have the same query_name
     # and that the order in query is preserved (i.e. that they're sorted by query_start)
@@ -58,50 +56,3 @@ def test_porec_iterator(namesorted_align_file):
         #sorted order
         assert( sorted(aligns,key=lambda x: x.query_alignment_start) == aligns)
 
-#signature: def map_to_fragments(input_bam: str, ref_file: str, output_file: str, method: str) -> None:
-def test_map_to_fragments(namesorted_align_file, hicREF_file, 'start'):
-    pass
-
-
-#def test_read_mappings_iter(namesorted_align_file):
-#    res = [len(aligns) for aligns in read_mappings_iter(namesorted_align_file)]
-#    assert(res == [2, 5, 6, 5, 2, 1])
-
-
-#def test_cluster_aligned_segments(namesorted_align_file):
-
-#    aligns = list(namesorted_align_file)[2:7]
-#    keep = cluster_aligned_segments(aligns, 20)
-#    assert(keep == [0, 1, 3])
-
-
-@pytest.mark.parametrize("pattern,rev_pattern",[
-    ('ACGT', 'ACGT'),
-    ('ACAGTA', 'TACTGT'),
-    ('(ACGT)', '(ACGT)'),
-    ('(A-CGT)', '(ACG-T)'),
-    ('AAGCTT', 'AAGCTT'),
-])
-def test_revcomp(pattern, rev_pattern):
-    assert(rev_pattern == revcomp(pattern))
-
-
-@pytest.mark.parametrize("pattern,regex",[
-    ('AAGCTT', '(AAGCTT|AAGCTT)'),
-    ('AAGNCTT', '(AAG[ACGT]CTT|AAG[ACGT]CTT)'),
-    ('(GAATTC|GCGGCCGC)', '((GAATTC|GCGGCCGC)|(GCGGCCGC|GAATTC))')
-])
-def test_create_regex(pattern, regex):
-    assert(regex == create_regex(pattern).pattern)
-
-
-@pytest.mark.parametrize("regex,seq,positions",[
-    ('(ACAGTA|TACTGT)', "ACAGTATACTGT", [0, 6]),
-    ('(ACAGTA|TACTGT)', "ACAGAATACAGT", []),
-    ('(AGCT|AAGCTT)', 'AAGCTT', [0]),
-    ('(AAGCTT|AAGCTT)', 'AAGCTT', [0]),
-    ('(AAGCTT|AAGCTT)', 'NAAGCTT', [1]),
-    ('(AAGCTT|AAGCTT)', 'AAGCTTAAGCTT', [0,6]),
-])
-def test_find_positions(regex, seq, positions):
-    assert(positions == find_site_positions(re.compile(regex), seq))
