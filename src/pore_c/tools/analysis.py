be8f1eb9b7fd11cdfcae48e4493938e780906f3c
Matthew Pendleton <matthew.pendleton@nanoporetech.com>
2019-04-08 22:40:06 -0400

    Added contact distance distribution plotting tool.

---
+++ src/pore_c/tools/analysis.py
@@ -0,0 +1,49 @@
+from collections import defaultdict, Counter
+
+import matplotlib
+matplotlib.use('agg')
+import matplotlib.pyplot as plt
+
+def plot_contact_distances(EC_matrix_file_in: str, graph_file_out: str, ref_bin_file: str) -> None:
+
+    chr_sizes =  {}
+
+    bin_size = False
+    for entry in open(ref_bin_file):
+        l = entry.strip().split()
+        if not bin_size:
+            bin_size = int(l[2]) - int(l[1])
+
+        if chr_sizes[l[0]] < l[3]:
+            chr_sizes[l[0]] = l[3]
+
+        min_max_size = len(min(chr_sizes.values()))
+
+    bin_data = {}
+    for entry in open(ref_bin_file):
+        l = entry.strip().split()
+        l[1] = int(l[1])
+        l[2] = int(l[2])
+        l[3] = int(l[3])
+        bin_data[int(l[3])] = l
+
+    data = Counter()
+
+    for entry in open(EC_matrix_file_in):
+        l = list(map(int,entry.strip().split()))
+        bin1 = bin_data[l[0]]
+        bin2 = bin_data[l[1]]
+        if bin1[0] == bin2[0]:
+            #intrachromosomal
+            if bin1[3] - bin2[3] <= max_bin_size:
+                data[bin_size * (bin1[3] - bin2[3])] += l[3]
+
+    fig, ax = plt.subplots(1,figsize=(12,6))
+    plt.subtitle("{} Contact Distance Distribution".format(EC_matrix_file_in))
+    distances, counts = zip(*sorted(data.items(),key = lambda x: x[0]))
+    distances_Mb = np.array(np.array(distances) / 10**6,dtype = int)
+    ax.plot(distances_Mb,counts)
+    ax.xlabel("distance (Mbp)", fontsize = "x-small")
+    ax.ylabel("Contacts", fontsize = "x-small")
+    ax.set_xlim(0,min_max_size * bin_size)
+    fig.savefig(graph_file_out)
