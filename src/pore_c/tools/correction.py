cf2930b054866f9e9edea7c2aa9beb458a7a7e5e
Matthew Pendleton <matthew.pendleton@nanoporetech.com>
2019-04-10 13:38:23 -0400

    No need to keep a copy of the original matrix in dense format. Keeping a sparse representation should be fine.

--- src/pore_c/tools/correction.py
+++ src/pore_c/tools/correction.py
@@ -4,14 +4,17 @@ from dataclasses import dataclass
 
 import scipy.stats as st
 import numpy as np
+import gzip
 
 @dataclass
 class HiCMap(object):
     def __init__(self, bin_ref):
         self.bin_count = 0
-        for entry in open(bin_ref):
+
+        for entry in gzip.open(bin_ref):
             self.bin_count += 1
 
+        self.sparse_data = {}
         self.matrix = np.ma.zeros((self.bin_count,self.bin_count),dtype = float)
         self.mask = None
         self.cP = None #np.copy(self.matrix)
@@ -30,6 +33,7 @@ class HiCMap(object):
                 continue
             l[0] = int(l[0])
             l[1] = int(l[1])
+            self.sparse_data[(l[0],l[1])] = l
             if tuple(sorted([l[0],l[1]])) in symmetry_test:
                 raise ValueError("This data set is not contact commutative. There are contacts such that there is an AB contact value that is not equal to BA contact value.")
             symmetry_test.add(tuple(sorted([l[0],l[1]])))
@@ -56,7 +60,7 @@ class HiCMap(object):
         
 
 
-    def set_thresholds(self, ci=0.9999, remove_zeros = True):
+    def set_thresholds(self, file_out, ci=0.9999, remove_zeros = True):
         """
         This protocol establishes a mask for the contact matrix
         such that values above the confidence interval are masked
@@ -70,10 +74,12 @@ class HiCMap(object):
         if remove_zeros:
             np.ma.masked_where(self.matrix == 0, self.matrix)
         
-        raw_values_mean = np.mean(self.matrix)
-
+        raw_values_mean = np.median(np.diag(self.matrix))
+        print(raw_values_mean)
+        print( st.t.interval(ci,raw_values_mean))
         #interval(alpha,df,loc=0,scale=1)
         self.min_val, self.max_val = st.t.interval(ci,raw_values_mean)
+        print (self.min_val, self.max_val)
         self.matrix[self.matrix < self.min_val] = np.ma.masked
         self.matrix[self.matrix > self.max_val] = np.ma.masked
 
@@ -93,6 +99,21 @@ class HiCMap(object):
 
         print(self.matrix)
 
+        import matplotlib
+        matplotlib.use('agg')
+        import matplotlib.pyplot as plt
+
+        fig, ax = plt.subplots(1,figsize = (12,6))
+        unique, counts = np.unique(self.matrix, return_counts=True)
+        ax.plot(unique,counts, 'k.')
+        ax.set_xlim(1,1000)
+        ax.set_ylim(0, 100)
+
+        ax.set_xlabel("total counts per bin")
+        ax.set_ylabel("frqeuency")
+
+        plt.savefig('count_distriubtion.png')
+
 
 
     def sinkhorn_knopp_correction(self, max_iter = 1000, epsilon = 10.**-6):
@@ -103,8 +124,10 @@ class HiCMap(object):
 
         #zeroeth iteration
         r = np.ones((self.bin_count,1))
-        
-        self.cP = np.copy(self.matrix)
+ 
+        #keeping an original copy of the matrix may be unnecessary
+#        self.cP = np.copy(self.matrix)
+        self.cP = self.matrix
 
         e_min = 1 - epsilon
         e_max = 1 + epsilon
@@ -132,8 +155,6 @@ class HiCMap(object):
 
         self.cP = d1.dot(self.matrix).dot(d2)
 
-#        print(iterations)
-
     def knight_ruiz_correction(self, remove_zeros = True, min = 1, max = 10**9, epsilon = 10.**-6):
         """
         This protocol uses the Knight-Ruiz iterative correction algorithm
@@ -144,6 +165,9 @@ class HiCMap(object):
         """
         raise NotImplemented
         
+    def calculate_eigenvectors(self):
+        raise NotImplemented
+
 
     def write_out_sparse_probability_matrix(self, matrix_file_out):
         if self.cP is None:
@@ -165,7 +189,7 @@ class HiCMap(object):
                         )
         f_out.close()
 
-def compute_contact_probabilities( matrix_file_in: str, bin_ref:str, corrected_matrix_file_out: str, output_eigenvector_data: str, correction_method: str, ci: Optional[float] = 0.999, mask_zeros: Optional[bool] = True,  epsilon: Optional[float] = 10**-3 , max_iter: Optional[float] = 1000) -> None:
+def compute_contact_probabilities( matrix_file_in: str, bin_ref:str, corrected_matrix_file_out: str, correction_method: str, ci: Optional[float] = 0.999, mask_zeros: Optional[bool] = True,  epsilon: Optional[float] = 10**-3 , max_iter: Optional[float] = 1000) -> None:
 
     #0 determine the size of the map from the reference file
     contact_matrix = HiCMap(bin_ref)
@@ -174,12 +198,14 @@ def compute_contact_probabilities( matrix_file_in: str, bin_ref:str, corrected_m
     contact_matrix.populate_matrix(matrix_file_in)
     
     #2. mask values higher and lower than the confidence interval
-    contact_matrix.set_thresholds(ci = ci)
+    contact_matrix.set_thresholds(corrected_matrix_file_out.replace(".corrected.matrix",'.png'),ci = ci)
     
     #4. write out corrected matrix file in the same sparse format as raw contact matrix
     if correction_method == "SK":
         contact_matrix.sinkhorn_knopp_correction(max_iter = max_iter, epsilon = epsilon )
+
     if correction_method == "KR":
         raise NotImplementedError
 
+    print(contact_matrix.cP)
     contact_matrix.write_out_sparse_probability_matrix(corrected_matrix_file_out)
