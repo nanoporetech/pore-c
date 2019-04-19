5c4e735d509f4962abfc6275c5c5b5326d58b4e5
Matthew Pendleton <matthew.pendleton@nanoporetech.com>
2019-04-19 11:02:38 -0400

    Added PCA support. Simplified matrix file loading into a single method. Added function that takes in multiple matrices and adds them together into a single matrix.

--- src/pore_c/tools/correction.py
+++ src/pore_c/tools/correction.py
@@ -15,14 +15,37 @@ class HiCMap(object):
             self.bin_count += 1
 
         self.sparse_data = {}
-        self.matrix = np.ma.zeros((self.bin_count,self.bin_count),dtype = float)
+#        self.matrix = np.zeros((self.bin_count,self.bin_count),dtype = float)
+        self.matrix = np.ma.identity(self.bin_count,dtype = float)
         self.mask = None
         self.cP = None #np.copy(self.matrix)
         self.min_val = np.min(self.matrix)
         self.max_val = np.max(self.matrix)
         self.total_contacts = 0
 
-    def populate_matrix(self, matrix_file):
+    def add_datum(self,x,y, value):
+        if x == y:
+            #protect from losing total support
+            if self.matrix[x,y] == 1:
+                self.matrix[x,y] = value
+            else:
+                self.matrix[x,y] += value
+        else:
+            self.matrix[x,y] += value
+            self.matrix[y,x] = self.matrix[x,y]
+
+    def from_raw_matrix_file(self, matrix_file):
+        for entry in open(matrix_file):
+            l = entry.strip().split()
+            l[0] = int(l[0])
+            l[1] = int(l[1])
+            l[2] = int(l[2])
+            if l[0] > l[1]:
+                self.matrix.add_datum(l[1], l[0], l[2])
+            else:
+                self.matrix.add_datum(*l)
+            
+
         symmetry_test = set()
 
         for entry in open(matrix_file):
@@ -33,31 +56,34 @@ class HiCMap(object):
                 continue
             l[0] = int(l[0])
             l[1] = int(l[1])
-            self.sparse_data[(l[0],l[1])] = l
-            if tuple(sorted([l[0],l[1]])) in symmetry_test:
+
+            ts = tuple(sorted([l[0],l[1]]))
+            if ts in symmetry_test:
                 raise ValueError("This data set is not contact commutative. There are contacts such that there is an AB contact value that is not equal to BA contact value.")
-            symmetry_test.add(tuple(sorted([l[0],l[1]])))
+            symmetry_test.add(ts)
             l[2] = float(l[2])
+            self.sparse_data[(l[0],l[1])] = l
             if l[0] == l[1]:
-                self.matrix[l[0],l[1]] = l[2]
+                ###very clever cheating is happening here: force the matrix to have total support by including the notion that
+                #  every bin on the genome is in contact with itself (i.e., add the constituted contact matrix with
+                #  a diagonal matrix whose value is all 1). >:)
+                self.matrix[l[0],l[1]] = np.max([self.matrix[l[0],l[1]],l[2]])
             else:
                 self.matrix[l[0],l[1]] = self.matrix[l[1],l[0]] = l[2] 
 
-        #confirm that the resulting matrix is a square, strictly positive matrix 
-        # total support is tested in the set_thresholds step
-        try:
-            assert self.matrix.ndim == 2
-            assert self.matrix.shape[0] == self.matrix.shape[1]
-            assert np.all(self.matrix >= 0)
-        except:
-            raise ValueError("Contact matrix is not square and strictly positive.")
+            #test for matrix square-ness
+ #           try:
+ #               assert self.matrix.ndim == 2
+ #               assert self.matrix.shape[0] == self.matrix.shape[1]
+ #           except:
+ #               raise ValueError("Contact matrix is not square.")
 
-        ###very clever cheating is happening here: force the matrix to have total support by including the notion that
-        #  every bin on the genome is in contact with itself (i.e., add the constituted contact matrix with 
-        #  a diagonal matrix whose value is all 1). >:)
-        diag = np.diag(np.ones(self.bin_count))
-        self.matrix = np.maximum( diag, self.matrix)
-        
+            #confirm that the resulting matrix is strictly positive matrix 
+            # total support is tested in the set_thresholds step
+            try:
+                assert np.all(self.matrix >= 0)
+            except:
+                raise ValueError("Contact matrix is not strictly positive.")
 
 
     def set_thresholds(self, file_out, ci=0.9999, remove_zeros = True):
@@ -112,7 +138,7 @@ class HiCMap(object):
         ax.set_xlabel("total counts per bin")
         ax.set_ylabel("frqeuency")
 
-        plt.savefig('count_distriubtion.png')
+        plt.savefig(file_out)
 
 
 
@@ -165,17 +191,17 @@ class HiCMap(object):
         """
         raise NotImplemented
         
-#    def calculate_eigenvectors(self):
-#        from sklearn.decomposition import PCA
-#        pca = PCA(n_components=2)
-#        self.pca = fit_transform(self.cP)
+    def calculate_eigenvectors(self):
+        from sklearn.decomposition import PCA
+        pca = PCA(n_components=2)
+        self.pca = pca.fit_transform(self.cP)
 
     def write_out_sparse_probability_matrix(self, matrix_file_out):
         if self.cP is None:
             raise ValueError("Sparse matrix hasn't been calculated yet.")
 
-        template = "{row} {column} {raw_counts} {contact_probability} {corrected_counts}\n"
-#        template = "{row} {column} {raw_counts} {contact_probability} {corrected_counts} {E1} {E2}\n"
+#        template = "{row} {column} {raw_counts} {contact_probability} {corrected_counts}\n"
+        template = "{row} {column} {raw_counts} {contact_probability} {corrected_counts} {E1} {E2}\n"
 
         f_out = open(matrix_file_out,'w')
         
@@ -190,23 +216,27 @@ class HiCMap(object):
                                             raw_counts = self.matrix[x,y], 
                                             contact_probability = self.cP[x,y], 
                                             corrected_counts = self.cP[x,y] * per_bin_contacts,
-                                            #                                        E1 = self.pca[x][0],
-                                            #                                        E2 = self.pca[x][1]
+                                            E1 = self.pca[x][0],
+                                            E2 = self.pca[x][1]
                                     )
                         )
         f_out.close()
 
-def compute_contact_probabilities( matrix_file_in: str, bin_ref:str, corrected_matrix_file_out: str, correction_method: str, ci: Optional[float] = 0.999, mask_zeros: Optional[bool] = True,  epsilon: Optional[float] = 10**-3 , max_iter: Optional[float] = 1000) -> None:
+def compute_contact_probabilities( matrix_file_in: str, bin_ref:str, matrix_file_out: str, correction_method: str, ci: Optional[float] = 0.999, mask_zeros: Optional[bool] = True,  epsilon: Optional[float] = 10**-3 , max_iter: Optional[float] = 1000, eigen: Optional[bool] = False) -> None:
 
     #0 determine the size of the map from the reference file
+    print("loading reference genome binning map...")
     contact_matrix = HiCMap(bin_ref)
 
+    print("loading raw contact data...")
     #1. read in matrix from raw contact count file
-    contact_matrix.populate_matrix(matrix_file_in)
+    contact_matrix.from_raw_matrix_file(matrix_file_in)
     
+    print("Thresholding data...")
     #2. mask values higher and lower than the confidence interval
-    contact_matrix.set_thresholds(corrected_matrix_file_out.replace(".corrected.matrix",'.png'),ci = ci)
+    contact_matrix.set_thresholds(matrix_file_out.replace(".corrected.matrix",'.png'),ci = ci)
     
+    print("performing correction...")
     #4. write out corrected matrix file in the same sparse format as raw contact matrix
     if correction_method == "SK":
         contact_matrix.sinkhorn_knopp_correction(max_iter = max_iter, epsilon = epsilon )
@@ -214,7 +244,24 @@ def compute_contact_probabilities( matrix_file_in: str, bin_ref:str, corrected_m
     if correction_method == "KR":
         raise NotImplementedError
 
-#    contact_matrix.calculate_eigenvectors()
+    if eigen:
+        print("Identifying principle components...")
+        contact_matrix.calculate_eigenvectors()
 
-    print(contact_matrix.cP)
-    contact_matrix.write_out_sparse_probability_matrix(corrected_matrix_file_out)
+    print("writing out corrected data...")
+    contact_matrix.write_out_sparse_probability_matrix(matrix_file_out)
+
+
+#takes in a set of contact matrix files and joins them together into a new .matrix file.
+# if they've been balanced, the resulting matrix can be balanced if specified.
+def join_contact_matrices( bin_ref_file_in, matrix_file_out, *matrix_filenames, correction=False):
+    outputted_matrix = HiCMap(bin_ref_file_in)
+    for filename in matrix_filenames:
+        #this works because raw matrix file loading is done using the add datum operation
+        outputted_matrix.from_raw_matrix_file(filename)
+
+    if correction:
+        outputted_matrix.sinkhorn_knopp_correction()
+
+    outputted_matrix.write_out_sparse_probability_matrix(matrix_file_out)
+    
