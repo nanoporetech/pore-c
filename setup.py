7dfde25d3100538db87f992f04b1a58340517f68
Eoghan Harrington <eoghan.harrington@nanoporetech.com>
2019-08-28 16:48:18 -0400

    Add conda build recipe
    Add versioneer so that we can get a reasonable version number and a conda build recipe

--- setup.py
+++ setup.py
@@ -3,48 +3,57 @@
 from __future__ import absolute_import
 from __future__ import print_function
 
-import io
-import re
-import os
-import codecs
-
-from glob import glob
-from os.path import basename
-from os.path import dirname
-from os.path import join
-from os.path import splitext
+import versioneer
 
 from setuptools import find_packages
 from setuptools import setup
 
+version = versioneer.get_version()
 
-here = os.path.abspath(os.path.dirname(__file__))
+python_requires = ">=3.6"
 
-def read(*parts):
-    with codecs.open(os.path.join(here, *parts), 'r') as fp:
-        return fp.read()
+install_requires = [
+	"biopython ",
+	"click >=7.0",
+	"cooler >=0.8.5",
+	"dask >=2.0.0",
+	"intake",
+	"intake-parquet",
+	"ncls",
+	"pairix",
+	"pairtools",
+	"pandas >=0.25",
+	"numpy >=1.16",
+	"pyarrow",
+	"pysam",
+	"tabix",
+	"tqdm",
+]
 
-def find_version(*file_paths):
-    version_file = read(*file_paths)
-    version_match = re.search(r"^__version__ = ['\"]([^'\"]*)['\"]",
-                              version_file, re.M)
-    if version_match:
-        return version_match.group(1)
-    raise RuntimeError("Unable to find version string.")
+extras_require = {
+	"tests": [
+		"pytest"
+	],
+	"build": [],
+	"doc": [
+		"sphinx <2.0",
+	]
+}
+
+extras_require['dev'] = extras_require['tests'] + extras_require['doc']  + ['black', 'isort', 'flake8']
 
 
 setup(
     name='pore_c',
-    version=find_version('src/pore_c/', 'version.py'),
+    version=version,
     license='MPL 2.0',
     description='Tools to process data from an ONT poreC run',
     long_description='',
     author='Matthew Pendleton',
     author_email='Matthew.Pendleton@nanoporetech.com',
     url='https://github.com/nanoporetech/pore_c',
-    packages=find_packages('src'),
+    packages=find_packages(where='src'),
     package_dir={'': 'src'},
-    py_modules=[splitext(basename(path))[0] for path in glob('src/*.py')],
     include_package_data=True,
     zip_safe=False,
     classifiers=[
@@ -62,20 +71,13 @@ setup(
     keywords=[
         # eg: 'keyword1', 'keyword2', 'keyword3',
     ],
-    install_requires=[
-        'click',
-    ],
-    tests_require=[
-        'pytest',
-    ],
-    extras_require={
-        # eg:
-        #   'rst': ['docutils>=0.11'],
-        #   ':python_version=="2.6"': ['argparse'],
-    },
+	python_requires=python_requires,
+    install_requires=install_requires,
+    tests_require=extras_require['tests'],
+    extras_require=extras_require,
     entry_points={
         'console_scripts': [
             'pore_c = pore_c.cli:cli',
         ]
-    },
+    }
 )
